109e882cd5bcdb9961edd7fad9fbf749454471c0
PierceLanternStudios <pierceclark07@gmail.com>
2025-06-07 14:53:15 -0400

    documentation

--- src/GameTile.tsx
+++ src/GameTile.tsx
@@ -1,8 +1,22 @@
 import { Dispatch } from "react";
 import { Action } from "./UseAppState";
 import { Board } from "./Board";
-import { render } from "@testing-library/react";
 
+/**
+ * gameTileButton
+ *
+ * A function that generates a button for each tile in the game board.
+ * This button will handle the click events to reveal or flag tiles
+ * based on the current game state.
+ * @param dispatch    The dispatch function called to update the game state
+ *                    when a button is clicked.
+ * @param board       The current board state to use when rendering the
+ *                    buttons.
+ * @param row         The row index of the tile in the board.
+ * @param col         The column index of the tile in the board.
+ * @returns           A button component for a specific tile in the game board
+ * *                  that handles click events to reveal or flag tiles.
+ */
 export default function gameTileButton(
   dispatch: Dispatch<Action>,
   board: Board,
@@ -25,6 +39,16 @@ export default function gameTileButton(
   );
 }
 
+/**
+ * renderTile
+ *
+ * A function that generates the display value for a tile in the game board.
+ * It checks if the tile has been revealed, flagged, or contains a mine.
+ * @param board       The current board state to use when rendering the tile.
+ * @param row         The row index of the tile in the board.
+ * @param col         The column index of the tile in the board.
+ * @returns           A string representing the display value of the tile.
+ */
 function renderTile(board: Board, row: number, col: number): string {
   if (board.display[row][col] !== -1) return board.display[row][col].toString();
   if (board.flags[row][col]) return "🚩";
